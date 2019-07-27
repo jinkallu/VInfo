5c992bfc5746f900d36375abd6e6f780dbce3edd
Jinesh <jinesh.bond@gmail.com>
2019-07-27 23:15:38 +0200

    added trackballs

--- src/front_end/python/www/main.py
+++ src/front_end/python/www/main.py
@@ -1,10 +1,13 @@
 from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 @app.route("/")
+
 def home():
-    return "Hello, World!"
+    user = {'username': 'FreeEdu'}
+    return render_template('index.html', title='Home', user=user)
     
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
