765a4194fff093e3ad05ce0646ea2ef02efe039a
Jinesh <jinesh.bond@gmail.com>
2019-08-03 16:55:27 +0200

    added test

---
+++ src/flaskr/tests/test.js
@@ -0,0 +1,10 @@
+describe('My test suit', () => {
+	it('`true` should be `true`', () => {
+		expect(true).toBe(true);
+	});
+
+	it('`false` should be `false`', () => {
+		expect(true).toBe(true);
+	});
+});
+
