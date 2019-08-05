5c084b10217a7972a09afb54c35fda191958a920
Jinesh <jinesh.bond@gmail.com>
2019-08-05 10:01:04 +0200

    added sidenav

---
+++ src/flaskr/svelte-app/src/Sidenav.svelte
@@ -0,0 +1,97 @@
+<script>
+	let flag = 0;
+	
+	function toggle() {
+		if(flag === 0){
+				flag = 1;
+				openNav();
+		}
+		else{
+			flag = 0;
+			closeNav();
+		}
+	}
+	/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+	function openNav() {
+		document.getElementById("mySidebar").style.width = "250px";
+		document.getElementById("main").style.marginLeft = "250px";
+	}
+
+	/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+	function closeNav() {
+		document.getElementById("mySidebar").style.width = "0";
+		document.getElementById("main").style.marginLeft = "0";
+	}
+</script>
+
+<style>
+/* The sidebar menu */
+.sidebar {
+  height: 100%; /* 100% Full-height */
+  width: 0; /* 0 width - change this with JavaScript */
+  position: fixed; /* Stay in place */
+  z-index: 1; /* Stay on top */
+  top: 0;
+  left: 0;
+  background-color: #111; /* Black*/
+  overflow-x: hidden; /* Disable horizontal scroll */
+  padding-top: 60px; /* Place content 60px from the top */
+  transition: 0.5s; /* 0.5 second transition effect to slide in the sidebar */
+}
+
+/* The sidebar links */
+.sidebar a {
+  padding: 8px 8px 8px 32px;
+  text-decoration: none;
+  font-size: 25px;
+  color: #818181;
+  display: block;
+  transition: 0.3s;
+}
+
+/* When you mouse over the navigation links, change their color */
+.sidebar a:hover {
+  color: #f1f1f1;
+}
+
+/* The button used to open the sidebar */
+.openbtn {
+  font-size: 20px;
+  cursor: pointer;
+  background-color: #111;
+  color: white;
+  padding: 10px 15px;
+  border: none;
+}
+
+.openbtn:hover {
+  background-color: #444;
+}
+
+/* Style page content - use this if you want to push the page content to the right when you open the side navigation */
+#main {
+  transition: margin-left .5s; /* If you want a transition effect */
+  padding: 20px;
+}
+
+/* On smaller screens, where height is less than 450px, change the style of the sidenav (less padding and a smaller font size) */
+@media screen and (max-height: 450px) {
+  .sidebar {padding-top: 15px;}
+  .sidebar a {font-size: 18px;}
+}
+	</style>
+
+<div id="mySidebar" class="sidebar">
+  <a href="hi">About</a>
+  <a href="ji">Services</a>
+  <a href="ki">Clients</a>
+  <a href="ku">Contact</a>
+</div>
+
+<div id="main">
+	<button  class="openbtn" on:click={toggle}>
+		&#9776;
+</button>
+  <h2>Collapsed Sidebar</h2>
+  <p>Content...</p>
+</div>
