608089bf0eb3165131b636407e7f5b778fc931f4
Jinesh <jinesh.bond@gmail.com>
2019-08-22 16:16:36 +0200

    adding apicalls login and listnodes AB#12

---
+++ src/sapper_svelte/src/components/apicalls/ListNodes.svelte
@@ -0,0 +1,16 @@
+<script>
+    import { onMount } from "svelte";
+     //let url = "http://0.0.0.0:5000/login?username=jinesh&password=jinesh&sessiondata=ppp";
+     let url = "http://0.0.0.0:5000/listnodes?session=ss&nodetype=TOP";
+     export let data = [];
+     export let flag = false;
+     onMount(async () =>  {
+              const response = await fetch(url, {method: 'POST'});
+              const json = await response.json();
+              data = json;
+
+              //console.log(data.parent.briefdesc);
+              flag = true;
+         }
+     );
+</script>
