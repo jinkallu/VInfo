04b73381f0bea7426f8e33321229d102634f3222
Jinesh <jinesh.bond@gmail.com>
2019-09-02 20:39:23 +0200

    AB#21 united sapper

---
+++ src/noder/src/components/navmenu/Search.svelte
@@ -0,0 +1,29 @@
+<script>
+
+</script>
+
+<style>
+    .searchform {
+        padding-left: 10px;
+        position: absolute;
+        left: 500px;
+        top: 10px;
+        float: right;
+        background-color: white;
+
+    }
+    .search {
+        position: relative;
+        padding-left: 10px;
+        border: 0;
+        outline: none;
+        box-shadow: none;
+    }
+</style>
+
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+<form class = "searchform" action="/search">
+    <i class="fa fa-search"></i>
+  <input class = "search" type="text" name="search" placeholder="Search.."><br>
+</form>
