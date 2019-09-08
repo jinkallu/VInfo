1a59ebf49a4c45eaa871c2bd8d8c8a761c6c6971
Jinesh <jinesh.bond@gmail.com>
2019-09-08 23:30:33 +0200

    AB#11 user management, added Login component

--- src/noder/src/components/navmenu/Search.svelte
+++ src/noder/src/components/navmenu/Search.svelte
@@ -15,16 +15,22 @@
         background-color: white;
     }
     .search {
-        padding: 1px;
         border: 0;
         outline: none;
         box-shadow: none;
+    }
+    .fa.fa-search{
+        color: black; 
+        border: 0;
+        outline: none;
+        box-shadow: none;
+        padding-right: 1em;
     }
 </style>
 
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 
 <form class = "searchform" action="/search">
-    <i style = "color: black; padding-top: 1em; padding-right: 1em" class="fa fa-search"></i>
-  <input class = "search" type="text" name="search" placeholder="Search.."><br>
+    <i class="fa fa-search"></i>
+    <input class = "search" type="text" name="search" placeholder="Search.."><br>
 </form>
---
+++ src/noder/src/components/navmenu/usrmgmt/Login.svelte
@@ -0,0 +1,16 @@
+<script>
+</script>
+
+<style>
+     .login{
+          color: white;
+          width: 4em;
+          align-content: center;
+          text-align: center;
+          cursor: pointer;
+     }
+</style>
+
+<div class = "login" on:click>
+     Login
+</div>
